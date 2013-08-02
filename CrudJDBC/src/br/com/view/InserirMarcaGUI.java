9e3838ee9cdcfded8bcb60d241346ffab6743e5c
anderson_feliciano <anderson_feliciano@10.1.48.194>
2013-08-02 15:28:00 -0300

    Criando view para inserir marca

--- CrudJDBC/src/br/com/view/InserirMarcaGUI.java
+++ CrudJDBC/src/br/com/view/InserirMarcaGUI.java
@@ -27,45 +27,57 @@ public class InserirMarcaGUI extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        txId = new javax.swing.JTextField();
         txNome = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         btLimpar = new javax.swing.JButton();
         btSalvar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setLayout(null);
+        jPanel1.add(txId);
+        txId.setBounds(100, 40, 250, 30);
+        jPanel1.add(txNome);
+        txNome.setBounds(100, 80, 250, 30);
 
-        jLabel1.setFont(new java.awt.Font("Verdana", 0, 11)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Verdana", 0, 14)); // NOI18N
         jLabel1.setText("Nome: ");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(50, 80, 50, 15);
-        jPanel1.add(txNome);
-        txNome.setBounds(100, 80, 250, 20);
+        jLabel1.setBounds(40, 80, 60, 30);
+
+        jLabel3.setFont(new java.awt.Font("Verdana", 0, 14)); // NOI18N
+        jLabel3.setText("Id:");
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(40, 40, 60, 30);
 
         jLabel2.setFont(new java.awt.Font("Verdana", 0, 11)); // NOI18N
-        jLabel2.setText("Inserir Marca");
+        jLabel2.setText(" Atualizar Marca");
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(50, 60, 90, 15);
+        jLabel2.setBounds(50, 20, 110, 15);
 
         btLimpar.setText("Limpar");
+        btLimpar.setBorder(null);
         jPanel1.add(btLimpar);
-        btLimpar.setBounds(390, 230, 63, 23);
+        btLimpar.setBounds(210, 140, 60, 50);
 
         btSalvar.setText("Salvar");
+        btSalvar.setBorder(null);
         jPanel1.add(btSalvar);
-        btSalvar.setBounds(470, 230, 63, 23);
+        btSalvar.setBounds(290, 140, 60, 50);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 547, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 389, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 294, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 219, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
@@ -110,7 +122,9 @@ public class InserirMarcaGUI extends javax.swing.JFrame {
     private javax.swing.JButton btSalvar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txId;
     private javax.swing.JTextField txNome;
     // End of variables declaration//GEN-END:variables
 }
