94aac34e0d91c2f4f2c9e43b0aa8b8f5b94ebebc
anderson_feliciano <anderson_feliciano@10.1.48.194>
2013-08-08 17:09:56 -0300

    Criando classe Principal

---
+++ CrudJDBC/src/br/com/modelo/app/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.modelo.app;
+
+import br.com.view.JanelaPrincipal;
+
+/**
+ *
+ * @author anderson_feliciano
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        JanelaPrincipal jp = new JanelaPrincipal();
+        jp.setVisible(true);
+    }
+}
